1973742f783c2356d87ae4818d326d223074f120
ishibshi.futos <ishibshi.futos@outlook.com>
2019-04-21 14:07:49 +0900

    add new files

---
+++ Assets/Scripts/MainMenuWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuWindow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void  OnGui() {
+        GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+        GUI.Button(new Rect(20, 20, 150, 30), "button");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
